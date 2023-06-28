51cc68a689ddb2739b9ee5b5a953fe55d57f4db0
Edgar Martínez <edgar.danmm@gmail.com>
2023-06-28 13:04:47 -0600

    Correccion navbar, efecto breathe y mejora de estilos

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -8,41 +8,40 @@ import '../css/Home.css'
 export function Home (){
     return (
         <>
-        <NavBar></NavBar>
-        <header className='header'>
-            <div className='mainHeader'>
-                <h1 className='mainTitle'>Encripta tu texto con uno de los siguientes algoritmos</h1>
-            </div>
-            <p className='content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </header>
-        <body className='body'>
-            <section className='CardsSection'>
-            <CardRight
-                title="Matriz transpuesta por clave"
-                text="En él, el mensaje original estará limitado a un rectángulo, de izquierda a derecha y de arriba hacia abajo. Después, se escoge una clave para asignar un número a cada columna del rectángulo para determinar el orden. El número correspondiente a la letra de la clave estará determinado por orden alfabético. Por ejemplo, si la palabra clave es CAT y el mensaje es THE SKY IS BLUE el proceso sería el siguiente:"
-                button={ <Button text="Probar" route="/TransposClave"></Button> }>    
-            </CardRight>
-            <CardLeft
-                title="Matriz transpuesta por columna"
-                text="En él, el mensaje original estará limitado a un rectángulo, de izquierda a derecha y de arriba hacia abajo. Después, se escoge una clave para asignar un número a cada columna del rectángulo para determinar el orden. El número correspondiente a la letra de la clave estará determinado por orden alfabético. Por ejemplo, si la palabra clave es CAT y el mensaje es THE SKY IS BLUE el proceso sería el siguiente:"
-                button={ <Button text="Probar" route="/TransposColumn"></Button> }>    
-            </CardLeft>
-            <CardRight
-                title="Matriz transpuesta por clave"
-                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                button={ <span>Proximamente...</span>}>    
-            </CardRight>
-            <CardLeft
-                title="Matriz transpuesta por columna"
-                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                button={ <span>Proximamente...</span> }>    
-            </CardLeft>
-            </section>
-        </body>
-        <footer>
-            <Footer></Footer>
-
-        </footer>
+            <NavBar></NavBar>
+            <header className='header'>
+                <div className='mainHeader'>
+                    <h1 className='mainTitle'>Encripta tu texto con uno de los siguientes algoritmos</h1>
+                </div>
+                <p className='content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </header>
+            <body className='body'>
+                <section className='CardsSection'>
+                <CardRight
+                    title="Matriz transpuesta por clave"
+                    text="En él, el mensaje original estará limitado a un rectángulo, de izquierda a derecha y de arriba hacia abajo. Después, se escoge una clave para asignar un número a cada columna del rectángulo para determinar el orden. El número correspondiente a la letra de la clave estará determinado por orden alfabético. Por ejemplo, si la palabra clave es CAT y el mensaje es THE SKY IS BLUE el proceso sería el siguiente:"
+                    button={ <Button text="Probar" route="/TransposClave"></Button> }>    
+                </CardRight>
+                <CardLeft
+                    title="Matriz transpuesta por columna"
+                    text="En él, el mensaje original estará limitado a un rectángulo, de izquierda a derecha y de arriba hacia abajo. Después, se escoge una clave para asignar un número a cada columna del rectángulo para determinar el orden. El número correspondiente a la letra de la clave estará determinado por orden alfabético. Por ejemplo, si la palabra clave es CAT y el mensaje es THE SKY IS BLUE el proceso sería el siguiente:"
+                    button={ <Button text="Probar" route="/TransposColumn"></Button> }>    
+                </CardLeft>
+                <CardRight
+                    title="Matriz transpuesta por clave"
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    button={ <span>Proximamente...</span>}>    
+                </CardRight>
+                <CardLeft
+                    title="Matriz transpuesta por columna"
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    button={ <span>Proximamente...</span> }>    
+                </CardLeft>
+                </section>
+            </body>
+            <footer>
+                <Footer></Footer>
+            </footer>
         </>
     )
 }

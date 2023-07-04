f9832aae3da35ef48dad53006fab75a930e34142
Edgar Martínez <edgar.danmm@gmail.com>
2023-07-04 12:02:57 -0600

    Codigo para consumir la api

--- src/views/TransposColumn.jsx
+++ src/views/TransposColumn.jsx
@@ -8,19 +8,33 @@ export function TransposColumn(){
 
     const [terminalLineData, setTerminalLineData] = useState();
 
+    const consumeColumn = async (terminalInput) => {
+        try {
+            const response = await fetch('http://localhost:8080/columncrypt/' + terminalInput);
+            let data = await response.text();
+            const terminalOutputRes = <TerminalOutput>{setTerminalLineData(data)}</TerminalOutput>
+            return terminalOutputRes;
+        }
+        catch (err) {
+            console.log(err);
+            const terminalOutputRes = <TerminalOutput>{setTerminalLineData("Ocurrio un error, vuelve a intentarlo")}</TerminalOutput>
+            return terminalOutputRes;
+        }
+    }
+
     return (
         <>
             <header>
                 <NavBar></NavBar>
             </header>
-            <body>
+            <div>
                 <div className="containerTerminal">
-                    <Terminal height="332px" name='Algoritmo de transposicion por columna' onInput={terminalInput => <TerminalOutput>{ setTerminalLineData(terminalInput) }</TerminalOutput> }>
+                    <Terminal height="332px" name='Algoritmo de transposicion por columna' onInput={ terminalInput => consumeColumn(terminalInput) }>
                         <TerminalOutput key={"WelcomeText"}>Introduce el texto que deseas encriptar</TerminalOutput>
                         { terminalLineData }
                     </Terminal>
                 </div>
-            </body>
+            </div>
             <footer>
                 <Footer></Footer>
             </footer>
